81d17a9b939872f1d3b26976ca7b935003178fcd
pavel <leskovecpavel7@gmail.com>
2019-01-24 20:39:48 +0300

    commit

--- api/script.js
+++ api/script.js
@@ -1,4 +1,7 @@
-function getWeatherOnMyLocation (){
+// const $ = require('jquery');
+
+
+function getWeatherOnMyLocation () {
  return navigator.geolocation.getCurrentPosition(function(location) {
 	var ab = location.coords.latitude;
 	var ba = location.coords.longitude;
@@ -28,47 +31,32 @@ function getWeatherOnMyLocation (){
 };
 
 function displayData (currentWeather){
-		// var div_city = document.createElement('div');
-		// div_city.className = "city_weather";
-		// div_city.innerHTML = currentWeather.location.name + " , " + currentWeather.location.country ;
-		// document.getElementById("weather_info").appendChild(div_city);
-	
-		// var div_temp = document.createElement('div');
-		// div_temp.className = "temp_weather";
-		// div_temp.innerHTML = currentWeather.current.temp_c + " °С";
-		// document.getElementById("weather_info").appendChild(div_temp);
-
-		// var div_temp_f = document.createElement('div');
-		// div_temp_f.className = "temp_weather_f";
-		// div_temp_f.innerHTML = currentWeather.current.temp_f + " F";
-		// document.getElementById("weather_info").appendChild(div_temp_f);
-		var a = document.createElement("table");
+		
+		var a = $.create("table");
 		
 		
-		var checkCity = document.body;
+		var checkCity = $("body");
 		var arr = checkCity.textContent;
 
 		if(arr.indexOf(currentWeather.location.name) === -1){
 			
 
-	
-		a.innerHTML = `<tr><td>${currentWeather.location.name}</td><td>${currentWeather.location.country}</td><td>${currentWeather.current.temp_c + " °С"}</td><td>${currentWeather.current.temp_f + " F"}</td></tr>`;
-		table.appendChild(a)}
+			$("#table").append(`<tr><td>${currentWeather.location.name}</td><td>${currentWeather.location.country}</td><td>${currentWeather.current.temp_c + " °С"}</td><td>${currentWeather.current.temp_f + " F"}</td></tr>`)
+		}
 		
   
 
 };
-
 function removeDispWeather(){
-	document.getElementById("weather_info").innerHTML="";	
+	$("#weather_info").html("");	
 	
 };
 
 function displayError(){
-	var div_error = document.createElement('div');
-	div_error.className = "Error";
-	div_error.innerHTML = "An error occured! Please try again" ;
-	document.getElementById("weather_info").appendChild(div_error);
+	var div_error = $.create('div').$(".Error").$("An error occured! Please try again");
+	
+	
+	$("#weather_info").append(div_error);
 };
 return sendRequest()
 });
@@ -94,119 +82,51 @@ function sendRequest2() {
 	newRequest.onerror = err => 		
 		console.error('Ошибка');
 		displayError();
-	;
+	
 
 	newRequest.send();
 
-};
+
 function displayData (currentWeather){
-	// var div_city = document.createElement('div');
-	// div_city.className = "city_weather";
-	// div_city.innerHTML = currentWeather.location.name + " , " + currentWeather.location.country ;
-	// document.getElementById("weather_info").appendChild(div_city);
-
-	// var div_temp = document.createElement('div');
-	// div_temp.className = "temp_weather";
-	// div_temp.innerHTML = currentWeather.current.temp_c + " °С";
-	// document.getElementById("weather_info").appendChild(div_temp);
-
-	// var div_temp_f = document.createElement('div');
-	// div_temp_f.className = "temp_weather_f";
-	// div_temp_f.innerHTML = currentWeather.current.temp_f + " F";
-	// document.getElementById("weather_info").appendChild(div_temp_f);
-	var a = document.createElement("table");
+	
+	var a = $.create("table");
 		
 		
-	var checkCity = document.body;
-	var arr = checkCity.textContent;
+		var checkCity = $("body");
+		var arr = checkCity.textContent;
 
-	if(arr.indexOf(currentWeather.location.name) === -1){
+		if(arr.indexOf(currentWeather.location.name) === -1){
 			
 
-	
-		a.innerHTML = `<tr><td>${currentWeather.location.name}</td><td>${currentWeather.location.country}</td><td>${currentWeather.current.temp_c + " °С"}</td><td>${currentWeather.current.temp_f + " F"}</td></tr>`;
-		table.appendChild(a)}
-	
-
+			$("#table").append(`<tr><td>${currentWeather.location.name}</td><td>${currentWeather.location.country}</td><td>${currentWeather.current.temp_c + " °С"}</td><td>${currentWeather.current.temp_f + " F"}</td></tr>`)
+		}
+		
+  
 
 };
-
 function removeDispWeather(){
-document.getElementById("weather_info").innerHTML="";	
-
+	$("#weather_info").html("");	
+	
 };
 
 function displayError(){
-var div_error = document.createElement('div');
-div_error.className = "Error";
-div_error.innerHTML = "An error occured! Please try again" ;
-document.getElementById("weather_info").appendChild(div_error);
+	var div_error = $.create('div').$(".Error").$("An error occured! Please try again");
+	
+	
+	$("#weather_info").append(div_error);
 };
+return sendRequest()
+};
+
+
+	
+
+
 function clearTable(){
-	document.getElementById("table").innerHTML="";	
+	$("#table").html("");	
 	
 	};
+
+
+
 	
-
-
-
-
-
-
-	function getJsonOfChoosenElementFromLocalStorage (idOfTheChosenElement) {
-            
-		if (localStorage.getItem('id'+ idOfTheChosenElement.toString()) === null) {
-			downloadJsonOfChosenElementFromServer(idOfTheChosenElement);
-		} else {
-			dropDataToTheTable(JSON.parse(localStorage.getItem('id'+ idOfTheChosenElement.toString())));
-		}
-	}
-
-	function downloadJsonOfChosenElementFromServer (idOfTheChosenElement) {
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET','id'+idOfTheChosenElement+'.json', true);
-		xhr.send();
-		xhr.onreadystatechange = () => {
-			if(xhr.readyState != 4) return;
-			if(xhr.status != 200) {
-				alert( xhr.status + ': ' + xhr.statusText );
-			} else {
-				saveToLocalStotage(JSON.parse(xhr.responseText), idOfTheChosenElement);
-			}
-		}
-	}
-
-	function saveToLocalStotage (response, id) {
-			localStorage.setItem('id'+id+'', JSON.stringify(response));
-		dropDataToTheTable(response);
-	}
-
-	function dropDataToTheTable (arrOfSingleEmployee) {
-		arrOfSingleEmployee.forEach(item => {
-			let arrayOfIdData = [];
-			arrayOfIdData.push(currentWeather.location.name, currentWeather.location.country, currentWeather.location.temp_c, currentWeather.location.temp_f);
-
-			let tbody = document.getElementsByTagName('tbody')[0];
-			const tr = document.createElement('tr');
-			tbody.appendChild(tr);
-			for (let numOfCell = 0; numOfCell < arrayOfIdData.length; numOfCell++) {
-				const td = document.createElement('td');
-				tr.appendChild(td);
-				td.innerText = arrayOfIdData[numOfCell];
-			}
-		});
-	}
-
-	function clearTableAndLocalStorage() {
-		localStorage.clear();
-		const tbody = document.getElementsByTagName('tbody')[0];
-		const firstTableRow = tbody.firstChild;
-		while(firstTableRow.nextSibling) {
-			tbody.removeChild(tbody.lastChild);
-		}
-		const arrOfSpans = document.querySelectorAll('span');
-		for (numOfSpan = 0; numOfSpan < arrOfSpans.length; numOfSpan++) {
-			let span = document.getElementsByTagName('span')[numOfSpan];
-			span.style.background = 'white';
-		}
-	}
--- async-development/script.js
+++ async-development/script.js
@@ -1,45 +1,114 @@
-function sendRequest() {
-			
-		var newRequest = new XMLHttpRequest();
+import './style.css';
 
-		newRequest.open("GET",
-		"http://api.apixu.com/v1/current.json?key=b06a844ff9b94ff08c6182022181912&q=" + document.getElementById('weatherField').value);
 
-		newRequest.onload = () => {
-				var currentWeather = JSON.parse(newRequest.responseText);
-				console.log(currentWeather);
-				removeDispWeather();
-				displayData(currentWeather);
-		};
+const mainForm = document.querySelector('#mainForm');
 
-		newRequest.onerror = err => 		
-			console.error('Ошибка');
-			//displayError();
-		;
+mainForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-		newRequest.send();
-	
-};
+  getWeatherOnMyLocation();
+});
 
-function displayData (currentWeather){
-		var div_city = document.createElement('div');
-		div_city.className = "city_weather";
-		div_city.innerHTML = currentWeather.location.name + " , " + currentWeather.location.country ;
-		document.getElementById("weather_info").appendChild(div_city);
-	
-		var div_temp = document.createElement('div');
-		div_temp.className = "temp_weather";
-		div_temp.innerHTML = currentWeather.current.temp_c + " °С";
-		document.getElementById("weather_info").appendChild(div_temp);
-};
 
-function removeDispWeather(){
-	document.getElementById("weather_info").innerHTML="";	
-};
+function getWeatherOnMyLocation() {
+  return window.navigator.geolocation.getCurrentPosition((location) => {
+    const ab = location.coords.latitude;
+    const ba = location.coords.longitude;
+    const sum = `${ab},${ba}`;
+	 function sendRequest() {
+      const newRequest = new XMLHttpRequest();
 
-function displayError(){
-	var div_error = document.ceateElement('div');
-	div_error.className = "Error";
-	div_error.innerHTML = "An error occured! Please try again" ;
-	document.getElementById("weather_info").appendChild(div_error);
-};
+      newRequest.open('GET', `http://api.apixu.com/v1/current.json?key=b06a844ff9b94ff08c6182022181912&q=${sum}`);
+
+      newRequest.onload = () => {
+        const currentWeather = JSON.parse(newRequest.responseText);
+        console.log(currentWeather);
+        removeDispWeather();
+        displayData(currentWeather);
+      };
+
+      newRequest.onerror = err => console.error('Ошибка');
+      displayError();
+
+
+      newRequest.send();
+    }
+
+    function displayData(currentWeather) {
+      const a = document.createElement('table');
+
+
+      const checkCity = document.body;
+      const arr = checkCity.textContent;
+
+      if (arr.indexOf(currentWeather.location.name) === -1) {
+        a.innerHTML = `<tr><td>${currentWeather.location.name}</td><td>${currentWeather.location.country}</td><td>${`${currentWeather.current.temp_c} °С`}</td><td>${`${currentWeather.current.temp_f} F`}</td></tr>`;
+        table.appendChild(a);
+      }
+    }
+
+    function removeDispWeather() {
+      document.getElementById('weather_info').innerHTML = '';
+    }
+
+    function displayError() {
+      const div_error = document.createElement('div');
+      div_error.className = 'Error';
+      div_error.innerHTML = 'An error occured! Please try again';
+      document.getElementById('weather_info').appendChild(div_error);
+    }
+    return sendRequest();
+  });
+}
+const secondForm = document.querySelector('#secondForm');
+
+secondForm.addEventListener('submit', (a) => {
+  a.preventDefault();
+
+  sendRequest2();
+});
+function sendRequest2() {
+  const newRequest = new XMLHttpRequest();
+
+  newRequest.open('GET',
+    `http://api.apixu.com/v1/current.json?key=b06a844ff9b94ff08c6182022181912&q=${document.getElementById('weatherField').value}`);
+
+  newRequest.onload = () => {
+    const currentWeather = JSON.parse(newRequest.responseText);
+    console.log(currentWeather);
+    removeDispWeather();
+    displayData(currentWeather);
+  };
+
+  newRequest.onerror = err => console.error('Ошибка');
+  displayError();
+
+
+  newRequest.send();
+}
+function displayData(currentWeather) {
+  const a = document.createElement('table');
+
+
+  const checkCity = document.body;
+  const arr = checkCity.textContent;
+
+  if (arr.indexOf(currentWeather.location.name) === -1) {
+    a.innerHTML = `<tr><td>${currentWeather.location.name}</td><td>${currentWeather.location.country}</td><td>${`${currentWeather.current.temp_c} °С`}</td><td>${`${currentWeather.current.temp_f} F`}</td></tr>`;
+    table.appendChild(a);
+  }
+}
+
+function removeDispWeather() {
+  document.getElementById('weather_info').innerHTML = '';
+}
+
+function displayError() {
+  const div_error = document.createElement('div');
+  div_error.className = 'Error';
+  div_error.innerHTML = 'An error occured! Please try again';
+  document.getElementById('weather_info').appendChild(div_error);
+}
+function clearTable() {
+  document.getElementById('table').innerHTML = '';
+}
